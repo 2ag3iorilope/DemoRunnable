99f51329514be713f791b128d3001b59f7f13b95
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-11-25 10:24:14 +0100

    Initial Commit

---
+++ DemoRunnable/src/DemoRunnable/DemoRunnable.java
@@ -0,0 +1,37 @@
+package DemoRunnable;
+
+public class DemoRunnable implements Runnable{
+    private String nombre;
+   
+    public DemoRunnable(String nom){
+        nombre=nom;
+    }
+   
+    public void run(){
+        try {
+            int x=(int)(Math.random()*5000);
+            Thread.sleep(x);
+            System.out.println("Soy: "+nombre+" He estado dormido "+ x +" milisegundos");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }   
+    }
+   
+    public static void main(String[] args) {
+        Thread t1= new Thread(new DemoRunnable("Ana"));
+        Thread t2= new Thread(new DemoRunnable("Luis"));
+        Thread t3= new Thread(new DemoRunnable("Jon"));
+       
+        t1.start();
+        t2.start();
+        t3.start();
+       
+        System.out.println("Final del main() y por tanto fin de programa");
+       
+    }
+   
+}
+/**
+ * Programa honek Hiru hari sortzen ditu Bakooitzari pertsona baten izena emanez
+ * Honen bidez ematen du pertsona abt lo egon dela x ms bitartean baina izen horiek Hariak dira
+ */
